46bd3434e15cc138d49737a23618c1a098571330
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2017-01-06 00:06:16 +0600

    Allow to place new planets by clicking on the game field (Closes #6)

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject planetTemplate;
+
+	void Update () {
+		const float MaximumRaycastDistance = 1000.0f;
+		const string RayHitGameObjectTag = "GameField";
+
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, MaximumRaycastDistance)) {
+				if (hit.transform.CompareTag (RayHitGameObjectTag)) {
+					Instantiate (planetTemplate, hit.point, Quaternion.identity);
+				}
+			}
+		}
+	}
+
+}
